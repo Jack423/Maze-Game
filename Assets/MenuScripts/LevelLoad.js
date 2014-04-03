86aa1e9bcb588462ea55262387f1a97a36bf9358
Jack Butler <guitar423@gmail.com>
2014-04-03 10:07:39 -0500

    Version 1.3 of the maze game. Fixed the menu and made a new look on the menu. I will be adding more menu options later and adding a teleport thing once you finish the level.

---
+++ Assets/MenuScripts/LevelLoad.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+//Specify level to load here.
+var Level = 0;
+
+function Start () {
+
+}
+ 
+function Update () {
+
+}
+
+function OnClick () {
+Application.LoadLevel (Level);
+}
